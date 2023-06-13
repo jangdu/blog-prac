a9e1fbc970692a64004bb8c27d371ef33c742616
jangdu <jjd0324@gmail.com>
2023-06-13 10:12:01 +0900

    feat: POST /posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const Posts = require("../schemas/posts");
+const { v4: uuidv4 } = require("uuid");
 const router = express.Router();
+const Posts = require("../schemas/posts");
 
 router.get("/", async (req, res) => {
   const posts = await Posts.find();
@@ -14,7 +15,25 @@ router.get("/", async (req, res) => {
     };
   });
 
-  res.json({ postsData: postsData });
+  res.json({ data: postsData });
+});
+
+router.post("/", async (req, res) => {
+  const { user, password, title, content } = req.body;
+
+  if (!(user && password && title && content)) {
+    return res.status(400).json({ errorMessage: "데이터 형식이 올바르지 않습니다." });
+  }
+  const createdPost = await Posts.create({
+    postId: uuidv4(),
+    user,
+    password,
+    title,
+    content,
+  });
+  console.log(createdPost);
+
+  res.json({ message: "게시글을 생성하였습니다." });
 });
 
 module.exports = router;
