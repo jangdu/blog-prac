180279d531d4a0fc71b82a45c98053437cd37163
jangdu <jjd0324@gmail.com>
2023-06-13 12:30:01 +0900

    feat: PUT /posts/:postId

--- routes/posts.js
+++ routes/posts.js
@@ -60,7 +60,31 @@ router.get("/:postId", async (req, res) => {
       },
     });
   } catch (error) {
-    console.error(error);
+    res.status(500).json({ message: "데이터 형식이 올바르지 않습니다." });
+  }
+});
+
+router.put("/:postId", async (req, res) => {
+  try {
+    const { postId } = req.params;
+    const { user, password, title, content } = req.body;
+
+    const post = await Posts.findOne({ postId });
+
+    if (!(user && password && title && content)) {
+      return res.status(400).json({ errorMessage: "데이터 형식이 올바르지 않습니다." });
+    }
+    if (!post) {
+      return res.status(404).json({ message: "게시글 조회에 실패하였습니다." });
+    }
+
+    if (post.password === password) {
+      await Posts.updateOne({ postId }, { $set: { title, content } });
+    } else {
+      return res.status(404).json({ message: "비밀번호가 올바르지 않음" });
+    }
+    res.json({ message: "게시글을 수정하였습니다." });
+  } catch (error) {
     res.status(500).json({ message: "데이터 형식이 올바르지 않습니다." });
   }
 });
